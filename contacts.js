b248ec0d9f541b01db9112ecd42d1a3ff5f5b69f
Andrii Chychkan <mywork1728@gmail.com>
2023-10-25 16:59:09 +0300

    added list and id

--- contacts.js
+++ contacts.js
@@ -1,14 +1,34 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-const go = () => {
-  console.log("ofofo");
-};
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+// function listContacts() {
+//   fs.readFile("db/contacts.json", "utf-8")
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+// }
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((item) => item.id === contactId);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 module.exports = {
-  go,
+  listContacts,
+  getContactById,
 };
-
-const contactsPath = path.basename("/db/contacts.json", ".json");
-
-function listContacts() {}
