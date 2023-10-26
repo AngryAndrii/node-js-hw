00fb9bd3f97fe56bed587dde8ec64999a3994e89
Andrii Chychkan <mywork1728@gmail>
2023-10-27 00:03:16 +0300

    added remove contacts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const contacts = require("./contacts");
 
-const invokeAction = async ({ action, id }) => {
+const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
@@ -9,15 +9,21 @@ const invokeAction = async ({ action, id }) => {
       const oneContact = await contacts.getContactById(id);
       return console.log(oneContact);
     case "add":
-      contacts.addContact(name, email, phone);
+      const newContact = await contacts.addContact(name, email, phone);
+      return console.log(newContact);
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      return console.log(removeContact);
   }
 };
 
-// invokeAction({ action: "list" });
-// invokeAction({ action: "get", id: "e6ywwRe4jcqxXfCZOj_1e" });
-invokeAction({
-  action: "add",
-  name: "Frog",
-  email: "froggy@mail.com",
-  phone: "1231233",
-});
+// invokeAction({
+//   action: "add",
+//   name: "Frog",
+//   email: "froggy@mail.com",
+//   phone: "1231233",
+// });
+
+// invokeAction({ action: "remove", id: "ppp" });
+
+invokeAction({ action: "list" });
