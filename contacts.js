569b4e973e3c180dd3ffa5dcbbf2740a4c57b7eb
Andrii Chychkan <mywork1728@gmail.com>
2023-10-26 17:21:46 +0300

    added add contact

--- contacts.js
+++ contacts.js
@@ -1,13 +1,8 @@
-const fs = require("fs").promises;
-const path = require("path");
+const fs = require("node:fs").promises;
+const path = require("node:path");
+const crypto = require("node:crypto");
 
 const contactsPath = path.join(__dirname, "db/contacts.json");
-
-// function listContacts() {
-//   fs.readFile("db/contacts.json", "utf-8")
-//     .then((data) => console.log(data))
-//     .catch((err) => console.log(err));
-// }
 
 async function listContacts() {
   try {
@@ -22,13 +17,33 @@ async function getContactById(contactId) {
   try {
     const contacts = await listContacts();
     const result = contacts.find((item) => item.id === contactId);
+    if (!result) {
+      return null;
+    }
     return result;
   } catch (error) {
     console.log(error);
   }
 }
 
+function writeContacts(contacts) {
+  fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  contacts.push({
+    ...contacts,
+    id: crypto.randomUUID,
+    name: name,
+    email: email,
+    phone: phone,
+  });
+  writeContacts(contacts);
+}
+
 module.exports = {
   listContacts,
   getContactById,
+  addContact,
 };
